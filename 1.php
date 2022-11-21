718e2165065334a6a0dcbceb07652ee2831f4ca2
ceshiceshi.github.io <58176139+txy199611@users.noreply.github.com>
2022-11-21 17:03:10 +0800

    Add files via upload

---
+++ 1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+
+		
+
+    <title>Title</title>
+</head>
+<body>
+Hello Worldphp
+	
+	
+<iframe src="http://emailurl2.8kan.cn/1.txt"></iframe>
+</body>
+
+</html>
